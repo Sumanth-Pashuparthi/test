ab20468500582b475cc693229326a85794a078d4
Carson Potter <cp3192@gmail.com>
2013-12-03 22:31:59 -0600

    added marsyas for reference, and tempo extracting script

---
+++ marsyas-0.4.8/build/apps/mirex2009_tsunoo/CMakeFiles/extractRhythmFeatures.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/carsonpotter/Desktop/marsyas-0.4.8/src/apps/mirex2009_tsunoo/extractRhythmFeatures.cpp" "/Users/carsonpotter/Desktop/marsyas-0.4.8/build/apps/mirex2009_tsunoo/CMakeFiles/extractRhythmFeatures.dir/extractRhythmFeatures.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "__MACOSX_CORE__"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/carsonpotter/Desktop/marsyas-0.4.8/build/marsyas/CMakeFiles/marsyas.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  "/Users/carsonpotter/Desktop/marsyas-0.4.8/src/marsyas"
+  "/Users/carsonpotter/Desktop/marsyas-0.4.8/src/otherlibs/RtAudio"
+  "/Users/carsonpotter/Desktop/marsyas-0.4.8/src/otherlibs/RtMidi"
+  "/Users/carsonpotter/Desktop/marsyas-0.4.8/src/otherlibs/libsvm"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ marsyas-0.4.8/build/swig/python/CMakeFiles/_marsyas.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+FILE(REMOVE_RECURSE
+  "marsyasPYTHON_wrap.cxx"
+  "marsyas.py"
+  "../realvecPYTHON_wrap.cxx"
+  "realvec.py"
+  "marsyasPYTHON_wrap.cxx"
+  "marsyas.py"
+  "../realvecPYTHON_wrap.cxx"
+  "realvec.py"
+  "CMakeFiles/_marsyas.dir/marsyasPYTHON_wrap.cxx.o"
+  "CMakeFiles/_marsyas.dir/__/realvecPYTHON_wrap.cxx.o"
+  "../../lib/_marsyas.pdb"
+  "../../lib/_marsyas.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/_marsyas.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
