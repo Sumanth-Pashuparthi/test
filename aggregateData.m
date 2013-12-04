76675d5a82f5a80cb9a40ae2521f0934daa33765
Josh Jacobson <joshjacobson14@gmail.com>
2013-12-03 23:32:25 -0600

    Updated Matlab tempo scraper and tempo data results

--- aggregateData.m
+++ aggregateData.m
@@ -19,9 +19,10 @@ tempo = zeros(8,100);
 bluesFileList = dir(bluesDirectory);
 fileCount = length(bluesFileList);
 count =1;
+
 for i = 1:fileCount
    if(~bluesFileList(i).isdir && ~strcmpi(bluesFileList(i).name, 'cmds') && ~strcmpi(bluesFileList(1),'.')) 
-       cmd = strcat('./tempo ', fullfile(bluesDirectory, bluesFileList(i).name));
+       cmd = ['./tempo ', fullfile(bluesDirectory, bluesFileList(i).name)];
        [~, cmdout] = system(cmd);
        tempo(1, count) = str2double(cmdout(length(cmdout)-2:length(cmdout-1)));
        count=count+1;
@@ -34,7 +35,7 @@ fileCount = length(countryFileList);
 count =1;
 for i = 1:fileCount
    if(~countryFileList(i).isdir && ~strcmpi(countryFileList(i).name, 'cmds') && ~strcmpi(countryFileList(1),'.')) 
-       cmd = strcat('./tempo ', fullfile(countryDirectory, countryFileList(i).name));
+       cmd = ['./tempo ', fullfile(countryDirectory, countryFileList(i).name)];
        [~, cmdout] = system(cmd);
        tempo(2, count) = str2double(cmdout(length(cmdout)-2:length(cmdout-1)));
        count=count+1;
@@ -47,7 +48,7 @@ fileCount = length(discoFileList);
 count =1;
 for i = 1:fileCount
    if(~discoFileList(i).isdir && ~strcmpi(discoFileList(i).name, 'cmds') && ~strcmpi(discoFileList(1),'.')) 
-       cmd = strcat('./tempo ', fullfile(discoDirectory, discoFileList(i).name));
+       cmd = ['./tempo ', fullfile(discoDirectory, discoFileList(i).name)];
        [~, cmdout] = system(cmd);
        tempo(3, count) = str2double(cmdout(length(cmdout)-2:length(cmdout-1)));
        count=count+1;
@@ -60,7 +61,7 @@ fileCount = length(hiphopFileList);
 count =1;
 for i = 1:fileCount
    if(~hiphopFileList(i).isdir && ~strcmpi(hiphopFileList(i).name, 'cmds') && ~strcmpi(hiphopFileList(1),'.')) 
-       cmd = strcat('./tempo ', fullfile(hiphopDirectory, hiphopFileList(i).name));
+       cmd = ['./tempo ', fullfile(hiphopDirectory, hiphopFileList(i).name)];
        [~, cmdout] = system(cmd);
        tempo(4, count) = str2double(cmdout(length(cmdout)-2:length(cmdout-1)));
        count=count+1;
@@ -73,7 +74,7 @@ fileCount = length(metalFileList);
 count =1;
 for i = 1:fileCount
    if(~metalFileList(i).isdir && ~strcmpi(metalFileList(i).name, 'cmds') && ~strcmpi(metalFileList(1),'.')) 
-       cmd = strcat('./tempo ', fullfile(metalDirectory, metalFileList(i).name));
+       cmd = ['./tempo ', fullfile(metalDirectory, metalFileList(i).name)];
        [~, cmdout] = system(cmd);
        tempo(5, count) = str2double(cmdout(length(cmdout)-2:length(cmdout-1)));
        count=count+1;
@@ -86,7 +87,7 @@ fileCount = length(popFileList);
 count =1;
 for i = 1:fileCount
    if(~popFileList(i).isdir && ~strcmpi(popFileList(i).name, 'cmds') && ~strcmpi(popFileList(1),'.')) 
-       cmd = strcat('./tempo ', fullfile(popDirectory, popFileList(i).name));
+       cmd = ['./tempo ', fullfile(popDirectory, popFileList(i).name)];
        [~, cmdout] = system(cmd);
        tempo(6, count) = str2double(cmdout(length(cmdout)-2:length(cmdout-1)));
        count=count+1;
@@ -99,7 +100,7 @@ fileCount = length(reggaeFileList);
 count =1;
 for i = 1:fileCount
    if(~reggaeFileList(i).isdir && ~strcmpi(reggaeFileList(i).name, 'cmds') && ~strcmpi(reggaeFileList(1),'.')) 
-       cmd = strcat('./tempo ', fullfile(reggaeDirectory, reggaeFileList(i).name));
+       cmd = ['./tempo ', fullfile(reggaeDirectory, reggaeFileList(i).name)];
        [~, cmdout] = system(cmd);
        tempo(7, count) = str2double(cmdout(length(cmdout)-2:length(cmdout-1)));
        count=count+1;
@@ -112,7 +113,7 @@ fileCount = length(rockFileList);
 count =1;
 for i = 1:fileCount
    if(~rockFileList(i).isdir && ~strcmpi(rockFileList(i).name, 'cmds') && ~strcmpi(rockFileList(1),'.')) 
-       cmd = strcat('./tempo ', fullfile(rockDirectory, rockFileList(i).name));
+       cmd = ['./tempo ', fullfile(rockDirectory, rockFileList(i).name)];
        [~, cmdout] = system(cmd);
        tempo(8, count) = str2double(cmdout(length(cmdout)-2:length(cmdout-1)));
        count=count+1;
@@ -121,9 +122,9 @@ end
 
 %here we write the tempo to a text file, for later conversion and use in
 %WEKA.
-fid = fopen('tempoData.txt', '%w');
+fid = fopen('tempoData.txt', 'w');
 for i=1:100
-    fprintf(fid, '%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f \n', tempo(i, 1), tempo(i, 2), tempo(i, 3), tempo(i, 4), tempo(i, 5), tempo(i, 6), tempo(i, 7), tempo(i, 8));
+    fprintf(fid, '%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f \n', tempo(1,i), tempo(2,i), tempo(3,i), tempo(4,i), tempo(5,i), tempo(6,i), tempo(7,i), tempo(8,i));
 end
 fclose(fid);
 end
